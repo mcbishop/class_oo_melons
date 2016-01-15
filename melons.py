19f930143709923bff9c10d57b82db944f0f844a
Hackbright Student <info@hackbrightacademy.com>
2016-01-15 12:58:36 -0800

    Added international fee to get_total function.

--- melons.py
+++ melons.py
@@ -4,22 +4,31 @@
 class AbstractMelonOrder(object):
     """A melon order"""
 
-    def __init__(self,species,qty):
+    def __init__(self,species,qty, order_type, tax):
         """Initialize melon order attributes"""
         
         self.species = species
         self.qty = qty
         self.shipped = False       
+        self.order_type = order_type
+        self.tax = tax
 
     def get_total(self):
         """Calculate price."""
 
         base_price = 5
-        
+        intl_fee = 0
+
         if self.species.lower() == "christmas":
-            base_price *= 3
+            base_price *= 1.50
+
+        if (self.qty < 10 and self.order_type == "international"):
+            intl_fee += 3.00
         
-        total = (1 + self.tax) * self.qty * base_price
+        total = ((1 + self.tax) * self.qty * base_price) + intl_fee
+        if intl_fee:
+            print intl_fee
+
 
         #if subclass is InternationalMelon:
         # and order is under
@@ -37,9 +46,9 @@ class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
 
     def __init__(self, species, qty):
-        super(DomesticMelonOrder,self).__init__(species,qty)
-        self.order_type = "domestic"
-        self.tax = 0.08
+        super(DomesticMelonOrder,self).__init__(species,qty, order_type="domestic", tax=0.08 )
+        # self.order_type = "domestic"
+        # self.tax = 0.08
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -47,10 +56,10 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes"""
-        super(InternationalMelonOrder,self).__init__(species,qty)
+        super(InternationalMelonOrder,self).__init__(species,qty, order_type="international",tax=0.17)
         self.country_code = country_code
-        self.order_type = "international"
-        self.tax = 0.17
+        # self.order_type = "international"
+        # self.tax = 0.17
 
     def get_country_code(self):
         """Return the country code."""
