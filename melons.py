d39d93b193138cb72bef8370e2d10c7639712330
Hackbright Student <info@hackbrightacademy.com>
2016-01-14 17:21:53 -0800

    Created class AbstractMelonOrder as parent to existing DomesticMelonOrder and InternationalMelonOrder methods. Refactored methods.

---
+++ melons.py
@@ -0,0 +1,49 @@
+"""Contains order classes"""
+
+
+class AbstractMelonOrder(object):
+    """A melon order"""
+
+    def __init__(self,species,qty):
+        """Initialize melon order attributes"""
+        
+        self.species = species
+        self.qty = qty
+        self.shipped = False       
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+        return total
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
+
+    def __init__(self, species, qty):
+        super(DomesticMelonOrder,self).__init__(species,qty)
+        self.order_type = "domestic"
+        self.tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes"""
+        super(InternationalMelonOrder,self).__init__(species,qty)
+        self.country_code = country_code
+        self.order_type = "international"
+        self.tax = 0.17
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
